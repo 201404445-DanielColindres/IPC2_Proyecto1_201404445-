79013e553dc5ce0170ed4bdf8138411a0082cb74
201404445-DanielColindres <66330665+201404445-DanielColindres@users.noreply.github.com>
2021-08-29 23:39:18 -0600

    Add files via upload
    Ultimo archivo que voy a subir del proyecto 1

--- Proyecto_1/Codio_fuente/menu.py
+++ Proyecto_1/Codio_fuente/menu.py
@@ -1,11 +1,23 @@
+from archivo import archivo
 
 opcion =0
+confirmarArchivo = False
+archivo = archivo()
 
-def cararArchivo():
-    pass
+def continuar(a): 
+    qwe = input(a)
 
+def cargarArchivo():
+    nuevoValor = archivo.carga()
+    if  nuevoValor != False:
+        continuar('Archivo Cargado con exito! ')
+        print(nuevoValor)
+        confirmarArchivo = True
+    else:
+        print('Paso algo inesperado')
+    
 def procesarArchivo():
-    pass
+    print(archivo.procesar())
 
 def escribirArchivo():
     pass
@@ -13,49 +25,51 @@ def escribirArchivo():
 def mostrarDatos():
     print('''
 
-    -----------------------------------------------------------------
-    Datos del estudiante:
-        > Daniel José Colindres Fuentes
-        > 201404445
-        > Introducción a la Programacion y Computación 2 sección "A"
-        > Ingenieria en Ciencias y Sistemas
-        > 4to Semestre
-    -----------------------------------------------------------------
+-----------------------------------------------------------------
+Datos del estudiante:
+    > Daniel José Colindres Fuentes
+    > 201404445
+    > Introducción a la Programacion y Computación 2 sección "A"
+    > Ingenieria en Ciencias y Sistemas
+    > 4to Semestre
+-----------------------------------------------------------------
     
     ''')
+    continuar('Presione enter para continuar')
 
 def generarGrafica():
     pass
 
 def menuPrincipal ():
     global opcion
-
-    print('''
-    -------------------------------------
-    Menú Principal:
-        1. Cargar Archivo
-        2. Procesar Archivo
-        3. Escribir Achivo Salida
-        4. Mostrar datos del estudiante
-        5. Generar Gráfica
-        6. Salir
-    -------------------------------------
-
-    ''')
-
-    opcion = int(input("Ingrese una opcion:\n"))
-
     while True:
+        
+        print('''
+-------------------------------------
+Menú Principal:
+    1. Cargar Archivo
+    2. Procesar Archivo
+    3. Escribir Achivo Salida
+    4. Mostrar datos del estudiante
+    5. Generar Gráfica
+    6. Salir
+-------------------------------------
+
+        ''')
+
+        opcion = int(input("Ingrese una opcion:\n"))
+        
         if opcion == 1:
-            pass
+            cargarArchivo()
         elif opcion == 2:
-            pass
+            terreno = input('Ingrese el terreno que desea calcular:\n')
+            procesarArchivo(terreno)
         elif opcion == 3:
-            pass
+            print("3")
         elif opcion == 4:
-            pass
+            mostrarDatos()
         elif opcion == 5:
-            pass
+            print("5")
         elif opcion == 6:
             break
         
--- Proyecto_1/Codio_fuente/pila.py
+++ Proyecto_1/Codio_fuente/pila.py
@@ -7,4 +7,15 @@ class pila:
         self.ultimo = None
         self.size = 0
 
-        
+    def apilar(self, valor):
+        nuevo = nodo(valor)
+        if(self.size == 0):
+            self.ultimo = nuevo
+            nuevo.siguiente = None
+            self.size = 1
+        else:
+            nuevo.siguiente = self.ultimo
+            self.ultimo = nuevo
+            self.size +=1
+
+   
